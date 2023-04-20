f30b1b2a66fce5c5c8b1573512355c5c2d5a5383
MichelJonkman <mj.99@live.nl>
2023-04-20 22:22:57 +0200

    Improvements

--- src/Providers/DbalSchemaServiceProvider.php
+++ src/Providers/DbalSchemaServiceProvider.php
@@ -3,6 +3,8 @@
 namespace MichelJonkman\DbalSchema\Providers;
 
 use Illuminate\Database\Console\Migrations\MigrateCommand;
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use MichelJonkman\DbalSchema\Console\MigrateSchemaCommand;
 use MichelJonkman\DbalSchema\Database\ConnectionManager;
@@ -12,20 +14,13 @@ class DbalSchemaServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->scoped(ConnectionManager::class);
-        $this->app->bind(MigrateCommand::class, function() {
-            echo '<pre>';
-            print_r(321);
-            echo '</pre>';
-            exit;
-        });
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
-                MigrateSchemaCommand::class,
-                MigrateCommand::class,
+                MigrateSchemaCommand::class
             ]);
         }
     }
